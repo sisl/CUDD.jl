62ea59600275bdced6cc8daa719a6e4a01dee276
Wenting Zhao <wentingzhao@outlook.com>
2017-07-03 12:25:09 -0500

    Updated the code to match with the new build.

--- src/CUDD.jl
+++ src/CUDD.jl
@@ -1,7 +1,12 @@
 module CUDD
+    cd("../deps/cudd-3.0.0/cudd/.libs")
 
-    # load the shared CUDD library
-    global const _LIB_CUDD = Libdl.find_library(collect(["libcudd.dylib"]))
+    if isempty(Libdl.find_library(collect(["libcudd.so"])))
+        warn("Could not communicate with the CUDD shared lib. Make sure it exists and that it is on the path.")
+    else
+        # load the shared CUDD library
+        global const _LIB_CUDD = Libdl.find_library(collect(["libcudd.so"]))
+    end
 
     mutable struct Manager
     end
