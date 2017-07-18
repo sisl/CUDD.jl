897f88c31a95b2059594e48fdac3cf8a82ef598d
Wenting Zhao <wentingzhao@outlook.com>
2017-07-17 22:53:25 -0500

    Fixed the path issue...

--- src/CUDD.jl
+++ src/CUDD.jl
@@ -1,20 +1,19 @@
 module CUDD
-    cd("../deps/cudd-3.0.0/cudd/.libs")
+    cudd_path = joinpath(dirname(@__FILE__), "..", "deps", "cudd-3.0.0", "cudd", ".libs")
 
-    if isempty(Libdl.find_library(collect(["libcudd.so"])))
-        warn("Could not communicate with the CUDD shared lib. Make sure it exists and that it is on the path.")
+    if isempty(Libdl.find_library(collect(["libcudd.so"]), collect([cudd_path])))
+        warn("Could not communicate with libcudd. Make sure it exists and that it is on the path.")
     else
         # load the shared CUDD library
-        global const _LIB_CUDD = Libdl.find_library(collect(["libcudd.so"]))
+        global const _LIB_CUDD = Libdl.find_library(collect(["libcudd.so"]), collect([cudd_path]))
+
+        include("ADD.jl")
+        include("ADD_apply.jl")
+        include("ADD_find.jl")
+        include("ADD_abs.jl")
+        include("ADD_inverse.jl")
+        include("ADD_ITE.jl")
+        include("ADD_negate.jl")
+        include("ADD_walsh.jl")
     end
-
-    include("ADD.jl")
-    include("ADD_apply.jl")
-    include("ADD_find.jl")
-    include("ADD_abs.jl")
-    include("ADD_inverse.jl")
-    include("ADD_ITE.jl")
-    include("ADD_negate.jl")
-    include("ADD_walsh.jl")
-
-end # end module
+end
