1009d6c82e0cb152d7c58d03ef7627bcd518ac5d
Koz Ross <koz.ross@retro-freedom.nz>
2018-05-30 10:24:48 +1200

    Replace dead vlsi.colorado.edu link with a live mirror, parallelize library build as much as the target system can manage, enable use of OS qsort instead

--- deps/build.jl
+++ deps/build.jl
@@ -1,11 +1,11 @@
 if is_unix()
-    download("ftp://vlsi.colorado.edu/pub/cudd-3.0.0.tar.gz", "cudd-3.0.0.tar.gz")
+    download("https://github.com/kozross/cudd/archive/v3.0.0.tar.gz", "cudd-3.0.0.tar.gz")
     run(`tar -zxvf cudd-3.0.0.tar.gz`)
     rm("cudd-3.0.0.tar.gz")
     cd("cudd-3.0.0/")
 
-    run(`./configure --enable-silent-rules --enable-shared --enable-obj`)
-    run(`make -j4 check`)
+    run(`./configure --enable-silent-rules --enable-shared --enable-obj --with-system-qsort`)
+    run(`make -j$Sys.CPU_CORES check`)
 
     # rename the shared library if it's ended with dylib...
     cd("cudd/.libs")
